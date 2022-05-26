e28b9b903b92be5e34181da32b2664e5f8fb30dc
park taehyun <xogus6510@kakao.com>
2022-05-26 22:35:43 +0900

    타임리프 확인

--- src/main/java/com/example/demo/JspprojectApplication.java
+++ src/main/java/com/example/demo/JspprojectApplication.java
@@ -4,8 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import test.controller.HomeController;
+
+import test.controller.HomeController;
+
 @SpringBootApplication
 @ComponentScan(basePackageClasses = TestController.class)
+@ComponentScan(basePackageClasses = HomeController.class)
 public class JspprojectApplication {
 
 	public static void main(String[] args) {
